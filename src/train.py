bc274a8524d8409f2629600eef9d17e79338b646
XiaoM96 <xiaothemao@gmail.com>
2026-01-11 17:30:17 +0100

    update with profiling

--- src/train.py
+++ src/train.py
@@ -1,10 +1,13 @@
 import argparse
+import torch
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.profilers import PyTorchProfiler
 
 from src.data import ECGDataModule
 from src.model import ECGClassifier
+
 
 def main(args):
     seed_everything(42)
@@ -36,6 +39,15 @@ def main(args):
         patience=5,
         mode="min"
     )
+    
+    # Profiler
+    profiler = PyTorchProfiler(
+        dirpath="profiler",
+        filename="perf_logs",
+        export_to_chrome=True,
+        row_limit=20,
+        sort_by_key="cpu_time_total",
+    )
 
     # Trainer
     trainer = Trainer(
@@ -44,6 +56,7 @@ def main(args):
         logger=TensorBoardLogger("lightning_logs", name="ecg_classification"),
         accelerator="auto",
         devices="auto",
+        profiler=profiler,
     )
 
     # Train
@@ -58,7 +71,7 @@ if __name__ == "__main__":
     # Data params
     parser.add_argument("--data_dir", type=str, default="data/time_series", help="Path to raw data")
     parser.add_argument("--processed_dir", type=str, default="data/processed", help="Path to save processed .pt files")
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
+    parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
     parser.add_argument("--num_workers", type=int, default=4, help="Number of dataloader workers")
     
     # Model params
